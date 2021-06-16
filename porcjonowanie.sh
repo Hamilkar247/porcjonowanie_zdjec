ac7d478fd614a8532d2ed71bc43c07a92fa8b670
Hamilkar247 <adonnibal96@gmail.com>
2021-06-16 20:57:43 +0200

    [WO-1] dzielenie pliku
    Signed-off-by: Hamilkar247 <adonnibal96@gmail.com>

--- porcjonowanie.sh
+++ porcjonowanie.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-echo "aha"
-
+var="/home/devel/porcjonowanie_zdjec/kozienice_map"
+sudo rm kozienice_map.png_*
+sudo split -b 200000 ../kozienice_map.png kozienice_map/kozienice_map.png_
